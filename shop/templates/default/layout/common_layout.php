30cb21a66f4c269ad146fa7de2150fab5bfc9bf4
siburuxue <siburuxue@sina.com>
2016-12-20 15:19:35 +0800

    将商品分类列表由hover改为click

--- shop/templates/default/layout/common_layout.php
+++ shop/templates/default/layout/common_layout.php
@@ -77,36 +77,42 @@ var COOKIE_PRE = '<?php echo COOKIE_PRE;?>';var _CHARSET = '<?php echo strtolowe
 <script type="text/javascript">
 var PRICE_FORMAT = '<?php echo $lang['currency'];?>%s';
 $(function(){
+	function removeActive(obj){
+		obj.removeClass("hover");
+		var cat_id = obj.attr("cat_id");
+		obj.find("div[cat_menu_id='"+cat_id+"']").hide();
+	}
 	//首页左侧分类菜单
-	$(".category ul.menu").find("li").each(
-		function() {
-			$(this).hover(
-				function() {
-				    var cat_id = $(this).attr("cat_id");
-					var menu = $(this).find("div[cat_menu_id='"+cat_id+"']");
-					menu.show();
-					$(this).addClass("hover");
-					if(menu.attr("hover")>0) return;
-					menu.masonry({itemSelector: 'dl'});
-					var menu_height = menu.height();
-					if (menu_height < 60) menu.height(80);
-					menu_height = menu.height();
-					var li_top = $(this).position().top;
-					if ((li_top > 60) && (menu_height >= li_top)) $(menu).css("top",-li_top+50);
-					if ((li_top > 150) && (menu_height >= li_top)) $(menu).css("top",-li_top+90);
-					if ((li_top > 240) && (li_top > menu_height)) $(menu).css("top",menu_height-li_top+90);
-					if (li_top > 300 && (li_top > menu_height)) $(menu).css("top",60-menu_height);
-					if ((li_top > 40) && (menu_height <= 120)) $(menu).css("top",-5);
-					menu.attr("hover",1);
-				},
-				function() {
-					$(this).removeClass("hover");
-				    var cat_id = $(this).attr("cat_id");
-					$(this).find("div[cat_menu_id='"+cat_id+"']").hide();
-				}
-			);
+	$(".category ul.menu li").click(function() {
+		$(this).siblings().each(function(){
+			$(this).removeClass("hover");
+			var cat_id = $(this).attr("cat_id");
+			$(this).find("div[cat_menu_id='"+cat_id+"']").hide(); 
+		});
+		if(!$(this).hasClass('hover')){
+			var cat_id = $(this).attr("cat_id");
+			var menu = $(this).find("div[cat_menu_id='"+cat_id+"']");
+			menu.show();
+			$(this).addClass("hover");
+			if(menu.attr("hover")>0) return;
+			menu.masonry({itemSelector: 'dl'});
+			var menu_height = menu.height();
+			if (menu_height < 60) menu.height(80);
+			menu_height = menu.height();
+			var li_top = $(this).position().top;
+			if ((li_top > 60) && (menu_height >= li_top)) $(menu).css("top",-li_top+50);
+			if ((li_top > 150) && (menu_height >= li_top)) $(menu).css("top",-li_top+90);
+			if ((li_top > 240) && (li_top > menu_height)) $(menu).css("top",menu_height-li_top+90);
+			if (li_top > 300 && (li_top > menu_height)) $(menu).css("top",60-menu_height);
+			if ((li_top > 40) && (menu_height <= 120)) $(menu).css("top",-5);
+			menu.attr("hover",1);
+		}else{
+			$(this).removeClass("hover");
+			var cat_id = $(this).attr("cat_id");
+			$(this).find("div[cat_menu_id='"+cat_id+"']").hide(); 
 		}
-	);
+	});
+	
 	$(".head-user-menu dl").hover(function() {
 		$(this).addClass("hover");
 	},
