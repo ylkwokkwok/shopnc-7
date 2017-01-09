9e953dd40aaadc19bcc233b180d2d37e008b7932
siburuxue <siburuxue@sina.com>
2017-01-09 10:16:48 +0800

    修改首页滚动图片

--- data/resource/web_config/focus.php
+++ data/resource/web_config/focus.php
@@ -1,18 +1,19 @@
 <?php defined('InShopNC') or exit('Access Invalid!');?>
 
   <ul id="fullScreenSlides" class="full-screen-slides">
-          <?php if (is_array($output['code_screen_list']['code_info']) && !empty($output['code_screen_list']['code_info'])) { ?>
+      <?php if (is_array($output['code_screen_list']['code_info']) && !empty($output['code_screen_list']['code_info'])) { ?>
           <?php foreach ($output['code_screen_list']['code_info'] as $key => $val) { ?>
-          <?php if (is_array($val) && $val['ap_id'] > 0) { ?>
-          <li ap_id="<?php echo $val['ap_id'];?>" color="<?php echo $val['color'];?>" style="background: <?php echo $val['color'];?> url('<?php echo UPLOAD_SITE_URL.'/'.$val['pic_img'];?>') no-repeat center top;background-size:1200px 100%">
-            <a href="<?php echo $val['pic_url'];?>" target="_blank" title="<?php echo $val['pic_name'];?>">&nbsp;</a></li>
-          <?php }else { ?>
-          <li style="background: <?php echo $val['color'];?> url('<?php echo UPLOAD_SITE_URL.'/'.$val['pic_img'];?>') no-repeat center top;background-size:100% 100%">
-            <a href="<?php echo $val['pic_url'];?>" target="_blank" title="<?php echo $val['pic_name'];?>">&nbsp;</a></li>
+              <?php if (is_array($val) && $val['ap_id'] > 0) { ?>
+                  <li ap_id="<?php echo $val['ap_id'];?>" color="<?php echo $val['color'];?>" style="background: <?php echo $val['color'];?> url('<?php echo UPLOAD_SITE_URL.'/'.$val['pic_img'];?>') no-repeat center top;background-size:1200px 100%">
+                      <a href="<?php echo $val['pic_url'];?>" target="_blank" title="<?php echo $val['pic_name'];?>">&nbsp;</a>
+                  </li>
+              <?php }else { ?>
+                  <li style="background: <?php echo $val['color'];?> url('<?php echo UPLOAD_SITE_URL.'/'.$val['pic_img'];?>') no-repeat center top;background-size:1200px 100%">
+                      <a href="<?php echo $val['pic_url'];?>" target="_blank" title="<?php echo $val['pic_name'];?>">&nbsp;</a>
+                  </li>
+              <?php } ?>
           <?php } ?>
-          <?php } ?>
-          <?php } ?>
-
+      <?php } ?>
   </ul>
 <!--  <div class="jfocus-trigeminy">-->
 <!--    <ul>-->
